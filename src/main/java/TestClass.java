4f9037e6912ea3961069b0abf09ad7f5edef3ad5
aditone <aditone2378@gmail.com>
2022-09-25 12:46:29 +0300

    Initial commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("wow_wow");
+        System.out.println("wow_wow");
+        System.out.println("wow_wow");
+    }
+}
